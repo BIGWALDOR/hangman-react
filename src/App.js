b681996ae56789171aa1cf65508561e76276e36b
Walid Bouladam <bouladam.walid86@gmail.com>
2021-11-09 22:32:22 +1300

    Added Popup and Notification components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header'
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
+import Popup from './components/Popup'
+import Notification from './components/Notification'
 
 function App() {
   return (

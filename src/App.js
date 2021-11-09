1b42ae72ab9a5d1fa1756e139fa73a672e64c07b
Walid Bouladam <bouladam.walid86@gmail.com>
2021-11-09 22:29:06 +1300

    Added WrongLetters and Word Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header'
 import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
 
 function App() {
   return (
     <>
       <Header />
-      <Figure /> 
+        <div className="game-container">
+          <Figure />
+          <WrongLetters />
+          <Word /> 
+        </div>
     </>
   );
 }
